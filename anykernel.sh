1c87287930581899c8fcf49393d3eec803578ef3
Odin1101 <Odin1101@github.com>
2021-01-01 21:31:16 -0800

    initial spectrum support and profiles

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Sofia
+kernel.string=Dark-Eclipse-Kernel-v2.0-beta
 do.devicecheck=0
 do.modules=0
 do.systemless=1
@@ -24,41 +24,34 @@ block=/dev/block/by-name/boot;
 is_slot_device=auto;
 ramdisk_compression=auto;
 
-
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
-# set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 755 755 $ramdisk/*;
 # set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
+ui_print " **********************disclaimer*********************"
+ui_print ""
+ui_print "  This kernel is a work in progress.";
+ui_print "  No guarantees or warranties are given or implied."
+ui_print "  you are choosing to make these modifications,"
+ui_print "  use at your own risk."
+ui_print ""
+ui_print " *****************************************************"
 
 ## AnyKernel install
 dump_boot;
 
-# begin ramdisk changes
-
-# init.rc
-# backup_file init.rc;
-# replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
-
-# init.tuna.rc
-# backup_file init.tuna.rc;
-# insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-# append_file init.tuna.rc "bootscript" init.tuna;
-
-# fstab.tuna
-# backup_file fstab.tuna;
-# patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-# patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-# patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-# append_file fstab.tuna "usbdisk" fstab;
-
-# end ramdisk changes
+if [ -d $ramdisk/.backup ]; then
 
 write_boot;
-## end install
 
+else
+
+abort "spectrum components in the zip require magisk to function";
+
+fi;
+## end install
